5ed22afa5e95a32754ddf785398c1c0d510cf27e
kohsuke <kohsuke@dev.java.net>
2006-12-30 15:52:06 +0000

    updated.

--- src/test/java/hudson/plugins/emma/EmmaBuildActionTest.java
+++ src/test/java/hudson/plugins/emma/EmmaBuildActionTest.java
@@ -6,11 +6,11 @@ package hudson.plugins.emma;
 public class EmmaBuildActionTest extends AbstractEmmaTestBase {
     public void testLoad() throws Exception {
         EmmaBuildAction r = EmmaBuildAction.load(null,getClass().getResourceAsStream("coverage.xml"));
-        assertEquals(r.classCoverage.getPercentage(),100);
-        assertEquals(r.lineCoverage.getPercentage(),64);
-        assertRatio(r.classCoverage, 185,185);
-        assertRatio(r.methodCoverage, 1345,2061);
-        assertRatio(r.blockCoverage, 44997,74846);
-        assertRatio(r.lineCoverage, 8346.3f,13135);
+        assertEquals(r.clazz.getPercentage(),100);
+        assertEquals(r.line.getPercentage(),64);
+        assertRatio(r.clazz, 185,185);
+        assertRatio(r.method, 1345,2061);
+        assertRatio(r.block, 44997,74846);
+        assertRatio(r.line, 8346.3f,13135);
     }
 }
