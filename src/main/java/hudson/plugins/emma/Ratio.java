c10d870a2221d80fe0c584043ece12e67e53affb
kohsuke <kohsuke@dev.java.net>
2006-12-30 00:39:40 +0000

    giving it a value class semantics.

--- src/main/java/hudson/plugins/emma/Ratio.java
+++ src/main/java/hudson/plugins/emma/Ratio.java
@@ -16,5 +16,28 @@ final class Ratio implements Serializable {
         this.denominator = denominator;
     }
 
+    public String toString() {
+        return numerator+"/"+denominator;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ratio ratio = (Ratio) o;
+
+        if (Float.compare(ratio.denominator, denominator) != 0) return false;
+        if (Float.compare(ratio.numerator, numerator) != 0) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = numerator != +0.0f ? Float.floatToIntBits(numerator) : 0;
+        result = 31 * result + denominator != +0.0f ? Float.floatToIntBits(denominator) : 0;
+        return result;
+    }
+
     private static final long serialVersionUID = 1L;
 }
