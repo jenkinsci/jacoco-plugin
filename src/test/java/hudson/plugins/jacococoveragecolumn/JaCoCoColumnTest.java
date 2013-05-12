48f81e3a63e189080e4230923e1d35446326fd6d
Dominik Stadler <dominik.stadler@gmx.at>
2013-05-12 19:49:14 +0200

    Enhance unit test to fully cover JaCoCoColumn

--- src/test/java/hudson/plugins/jacococoveragecolumn/JaCoCoColumnTest.java
+++ src/test/java/hudson/plugins/jacococoveragecolumn/JaCoCoColumnTest.java
@@ -1,14 +1,29 @@
 package hudson.plugins.jacococoveragecolumn;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
+import hudson.console.ConsoleNote;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.model.Cause;
+import hudson.model.Descriptor.FormException;
+import hudson.model.ExternalJob;
+import hudson.model.ExternalRun;
 import hudson.model.Job;
+import hudson.plugins.jacoco.JacocoBuildAction;
+import hudson.search.QuickSilver;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.util.List;
+
+import javax.servlet.ServletContext;
 
 import org.easymock.classextension.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
+import org.kohsuke.stapler.export.Exported;
 
 public class JaCoCoColumnTest {
 
@@ -22,12 +37,123 @@ public class JaCoCoColumnTest {
 
 	@Test
 	public void testGetPercentWithNoLastSuccessfulBuild() {
-		final Job<?, ?> mockJob = EasyMock.createMock(Job.class);
-		EasyMock.expect(mockJob.getLastSuccessfulBuild()).andReturn(null);
-		EasyMock.replay(mockJob);
+		ServletContext context = EasyMock.createNiceMock(ServletContext.class);
+		
+		EasyMock.replay(context);
+
+		final Job<?, ?> mockJob = new ExternalJob("externaljob") {
+			@Override
+			protected void reload() {
+			}
+		};
 		assertEquals("N/A", jacocoColumn.getPercent(mockJob));
+		assertEquals(new BigDecimal("0.0"), jacocoColumn.getLineCoverage(mockJob));
+
+		EasyMock.verify(context);
 	}
 
+	@Test
+	public void testGetPercentWithLastSuccessfulBuild() {
+		ServletContext context = EasyMock.createNiceMock(ServletContext.class);
+		
+		EasyMock.replay(context);
+
+		final Job<?, ?> mockJob = new ExternalJob("externaljob") {
+			@Override
+			protected void reload() {
+			}
+
+			@Override
+			@Exported
+			@QuickSilver
+			public ExternalRun getLastSuccessfulBuild() {
+				try {
+					return newBuild();
+				} catch (IOException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+
+			@Override
+			protected synchronized void saveNextBuildNumber() throws IOException {
+			}
+		};
+		assertEquals("N/A", jacocoColumn.getPercent(mockJob));
+		assertEquals(new BigDecimal("0.0"), jacocoColumn.getLineCoverage(mockJob));
+
+		EasyMock.verify(context);
+	}
+
+	@Test
+	public void testGetPercentWithBuildAndAction() {
+		ServletContext context = EasyMock.createNiceMock(ServletContext.class);
+		
+		EasyMock.replay(context);
+
+		final Job<?, ?> mockJob = new ExternalJob("externaljob") {
+			@Override
+			protected void reload() {
+			}
+
+			@Override
+			@Exported
+			@QuickSilver
+			public ExternalRun getLastSuccessfulBuild() {
+				try {
+					ExternalRun newBuild = newBuild();
+					newBuild.getActions().add(new JacocoBuildAction(null, null, null, null, new BuildListener() {
+						private static final long serialVersionUID = 1L;
+
+						public void hyperlink(String url, String text) throws IOException {
+						}
+						
+						public PrintStream getLogger() {
+							return null;
+						}
+						
+						public PrintWriter fatalError(String format, Object... args) {
+							return null;
+						}
+						
+						public PrintWriter fatalError(String msg) {
+							return null;
+						}
+						
+						public PrintWriter error(String format, Object... args) {
+							return null;
+						}
+						
+						public PrintWriter error(String msg) {
+							return null;
+						}
+						
+						public void annotate(@SuppressWarnings("rawtypes") ConsoleNote ann) throws IOException {
+							
+						}
+						
+						public void started(List<Cause> causes) {
+						}
+						
+						public void finished(Result result) {
+						}
+					}, null, null));
+					assertEquals(1, newBuild.getActions().size());
+					return newBuild;
+				} catch (IOException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+
+			@Override
+			protected synchronized void saveNextBuildNumber() throws IOException {
+			}
+		};
+		assertEquals("0.0", jacocoColumn.getPercent(mockJob));
+		assertEquals(new BigDecimal("0.0"), jacocoColumn.getLineCoverage(mockJob));
+		
+		EasyMock.verify(context);
+	}
+	
 	@Test
 	public void testGetLineColorWithNull() throws Exception {
 		assertNull(jacocoColumn.getLineColor(null));
@@ -36,7 +162,6 @@ public class JaCoCoColumnTest {
 	@Test
 	public void testGetLineColor() throws Exception {
 		assertEquals("EEEEEE", jacocoColumn.getLineColor(BigDecimal.valueOf(100)));
-
 	}
 
 	@Test
@@ -47,7 +172,12 @@ public class JaCoCoColumnTest {
 	@Test
 	public void testGetFillColor100() throws Exception {
 		assertEquals("008B00", jacocoColumn.getFillColor(BigDecimal.valueOf(100)));
-
 	}
 
+	@Test
+	public void testDescriptor() throws FormException {
+		assertNotNull(jacocoColumn.getDescriptor());
+		assertNotNull(jacocoColumn.getDescriptor().newInstance(null, null));
+		assertNotNull(jacocoColumn.getDescriptor().getDisplayName());
+	}
 }
