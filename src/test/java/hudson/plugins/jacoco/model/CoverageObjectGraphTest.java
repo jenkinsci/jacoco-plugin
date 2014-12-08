0ba094cfd50197e9ecfff1aea3c4365dd29429d4
Martin Heinzerling <webmaster@mheinzerling.de>
2014-12-08 07:55:56 +0100

    JENKINS-16580: make test system independent

--- src/test/java/hudson/plugins/jacoco/model/CoverageObjectGraphTest.java
+++ src/test/java/hudson/plugins/jacoco/model/CoverageObjectGraphTest.java
@@ -3,10 +3,18 @@ package hudson.plugins.jacoco.model;
 import static org.junit.Assert.assertArrayEquals;
 
 import hudson.plugins.jacoco.AbstractJacocoTestBase;
+import hudson.plugins.jacoco.model.CoverageGraphLayout.CoverageType;
+import hudson.plugins.jacoco.model.CoverageGraphLayout.CoverageValue;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 import org.apache.commons.io.FileUtils;
 import org.easymock.EasyMock;
 import org.easymock.IMocksControl;
@@ -14,6 +22,7 @@ import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -23,20 +32,34 @@ public class CoverageObjectGraphTest extends AbstractJacocoTestBase
 {
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 200;
+	private static Font font;
 	private IMocksControl ctl;
+	private Locale localeBackup;
+
+	@BeforeClass
+	public static void loadFont() throws IOException, FontFormatException
+	{
+		// just a free font nobody has on their system, but different enough to default sans-serif,
+		// that you will see missing system font replacement in the outbut. See #replaceFonts()
+		InputStream is = new FileInputStream("resources/test/belligerent.ttf");
+		font=Font.createFont(Font.TRUETYPE_FONT, is);
+	}
 
 	@Before
-	public void prepareMock()
+	public void setUp()
 	{
 		ctl = EasyMock.createControl();
 		TestCoverageObject.setEasyMock(ctl);
+		localeBackup=Locale.getDefault();
+		Locale.setDefault(Locale.ENGLISH);
 	}
 
 	@After
-	public void verifyMock()
+	public void tearDown()
 	{
 		ctl.verify();
 		TestCoverageObject.setEasyMock(null);
+		Locale.setDefault(localeBackup);
 	}
 
 	@Test
@@ -57,6 +80,7 @@ public class CoverageObjectGraphTest extends AbstractJacocoTestBase
 
 	private void assertGraph(JFreeChart chart, String file, boolean writeFile) throws IOException
 	{
+		replaceFonts(chart);
 		if (writeFile)
 		{
 			File f = new File(file);
@@ -93,4 +117,28 @@ public class CoverageObjectGraphTest extends AbstractJacocoTestBase
 	{
 		assertGraph(chart, file, !new File("resources/test/" + file).exists());
 	}
+
+	private void replaceFonts(JFreeChart chart)
+	{
+		int i=0;
+		while (chart.getLegend(i)!=null)
+		{
+			chart.getLegend(i).setItemFont(font.deriveFont(chart.getLegend(i).getItemFont().getStyle(), chart.getLegend(i).getItemFont().getSize()));
+			i++;
+		}
+		i=0;
+		while (chart.getCategoryPlot().getDomainAxis(i)!=null)
+		{
+			chart.getCategoryPlot().getDomainAxis(i).setTickLabelFont(font.deriveFont(chart.getCategoryPlot().getDomainAxis(i).getTickLabelFont().getStyle(), chart.getCategoryPlot().getDomainAxis(i).getTickLabelFont().getSize()));
+			i++;
+		}
+		i=0;
+		while (chart.getCategoryPlot().getRangeAxis(i)!=null)
+		{
+			chart.getCategoryPlot().getRangeAxis(i).setTickLabelFont(font.deriveFont(chart.getCategoryPlot().getRangeAxis(i).getTickLabelFont().getStyle(), chart.getCategoryPlot().getRangeAxis(i).getTickLabelFont().getSize()));
+			chart.getCategoryPlot().getRangeAxis(i).setLabelFont(font.deriveFont(chart.getCategoryPlot().getRangeAxis(i).getLabelFont().getStyle(), chart.getCategoryPlot().getRangeAxis(i).getLabelFont().getSize()));
+			i++;
+		}
+	}
+
 }
